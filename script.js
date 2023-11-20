ab4fb763991630663b0857771806726fdaa8d0ae
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-11-20 21:13:03 +0100

    darkmode

--- script.js
+++ script.js
@@ -1,42 +1,19 @@
-// const navToggle = document.querySelector("#navToggle");
-// const navClosedIcon = document.querySelector("#navClosed");
-// const navOpenIcon = document.querySelector("#navOpen");
-// const navIcon = document.querySelectorAll(".navIcon");
-// const nav = document.querySelector("nav");
+var glideMulti1 = new Glide(".multi1", {
+  type: "carousel",
+  autoplay: 3500,
+  perView: 3,
+  breakpoints: {
+    1400: {
+      perView: 2,
+    },
+    800: {
+      perView: 1,
+    },
+  },
+  peek: {
+    before: 0,
+    after: 50,
+  },
+});
 
-// navToggle.addEventListener("click", () => {
-//   nav.classList.toggle("open");
-//   navIcon.forEach((icon) => {
-//     icon.classList.toggle("hidden");
-//   });
-// });
-
-// window.addEventListener(
-//   "resize",
-//   () => {
-//     if (document.body.clientWidth > 720) {
-//       nav.classList.remove("open");
-//       navIcon.forEach((icon) => {
-//         icon.classList.remove("hidden");
-//       });
-//       navOpenIcon.classList.add("hidden");
-//     }
-//   },
-//   { passive: false }
-// );
-
-// $(document).on("scroll", function () {
-//   var pageTop = $(document).scrollTop();
-//   var pageBottom = pageTop + $(window).height();
-//   var tags = $(".tag");
-
-//   for (var i = 0; i < tags.length; i++) {
-//     var tag = tags[i];
-
-//     if ($(tag).position().top < pageBottom) {
-//       $(tag).addClass("visible");
-//     } else {
-//       $(tag).removeClass("visible");
-//     }
-//   }
-// });
+glideMulti1.mount();
