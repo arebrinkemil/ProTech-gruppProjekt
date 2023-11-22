a2dae5b70ca310547624a7f54b65dd4cab9bac16
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-11-22 10:03:43 +0100

    compressed file

--- script.js
+++ script.js
@@ -25,31 +25,26 @@ var glideMulti1 = new Glide(".multi1", {
 
 glideMulti1.mount();
 
-// Store the time when the script is first run
 const startTime = Date.now();
 
 document.onreadystatechange = function () {
   if (document.readyState === "complete") {
     var loadingScreen = document.querySelector("#loadingScreen");
 
-    // Calculate how long the loading screen has been displayed
     var elapsedTime = Date.now() - startTime;
 
-    // Check if the elapsed time is less than 5 seconds
     if (elapsedTime < 5000) {
-      // Set a timeout to make up the remaining time
       setTimeout(function () {
         loadingScreen.classList.add("slide-up");
         setTimeout(function () {
           loadingScreen.parentNode.removeChild(loadingScreen);
-        }, 500); // match this with your CSS animation duration
-      }, 5000 - elapsedTime);
+        }, 500);
+      }, 2000 - elapsedTime);
     } else {
-      // If 5 seconds have already passed, hide the loading screen immediately
       loadingScreen.classList.add("slide-up");
       setTimeout(function () {
         loadingScreen.parentNode.removeChild(loadingScreen);
-      }, 500); // match this with your CSS animation duration
+      }, 500);
     }
   }
 };
